0717527a349f45f1630ea22f97a37f2f9123c63e
marimunari <marianamunari4@gmail.com>
2025-04-22 23:17:52 -0300

    feat: fixing user model

--- src/@core/models/User/user.model.ts
+++ src/@core/models/User/user.model.ts
@@ -1,7 +1,7 @@
 // Interface for defining the props of the User component
 export interface UserModel {
   /** Username to be displayed */
-  userName: string;
+  userName?: string | null;
 
   /** Optional image URL for the user's profile picture */
   userImage?: string  | null;
